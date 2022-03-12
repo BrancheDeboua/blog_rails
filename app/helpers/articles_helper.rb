b4c49e2bfdb0e758ed4d3389d6eda7eb0fd64ce1
Samuel Laforme <laforme.s@hotmail.com>
2022-03-11 22:01:09 -0500

    control the visual of delete and edit article

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+    def get_date(date)
+        date.strftime '%d %B %Y'
+    end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+    def get_date(date)
+        date.strftime '%d %B %Y'
+    end
+
+    def is_author(id)
+        if user_signed_in?
+            if current_user.id == id
+                true
+            else
+                false
+            end
+        end
+    end
 end
