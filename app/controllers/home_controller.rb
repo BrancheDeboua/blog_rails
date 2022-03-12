3c5822a92fbaa1d186b3e3eb17af827b2e77771b
Samuel Laforme <laforme.s@hotmail.com>
2022-03-11 21:22:19 -0500

    started adding some logic behind visuals

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,16 @@ class HomeController < ApplicationController
   def index
     @authors = Author.all
     @articles = Article.all.order("created_at DESC")
+    @is_author = false
+
+    if user_signed_in?
+      @authors.each do |author|
+        if author.id == current_user.id
+          @is_author = true
+        end
+      end
+    end
+
   end
 
   def about
