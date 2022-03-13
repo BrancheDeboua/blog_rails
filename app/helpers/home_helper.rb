67b78ac8413a6eb3f668479347c313f96c0c14c4
Samuel Laforme <laforme.s@hotmail.com>
2022-03-12 21:05:03 -0500

    show author name if author in sidebar

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,7 +3,7 @@ module HomeHelper
         date.strftime '%d %B %Y'
     end
 
-    def is_author(id)
+    def is_author_of_article(id)
         if user_signed_in?
             if current_user.id == id
                 true
@@ -11,5 +11,15 @@ module HomeHelper
                 false
             end
         end
+    end
+
+    def is_author(id)
+        author = Author.where('users_id = ?', id)
+        return !author.blank?
+    end
+
+    def get_author_name(id)
+        author = Author.where('users_id = ?', id)[0]
+        return "#{author.first_name} #{author.last_name}"
     end
 end
