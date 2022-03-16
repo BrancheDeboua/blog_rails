be5d58f2d19a58406330ab12bcea187b808c99a4
Samuel Laforme <laforme.s@hotmail.com>
2022-03-15 22:01:22 -0400

    started working on sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,11 @@ class HomeController < ApplicationController
     @authors = Author.all
     @articles = Article.all.order("created_at DESC")
     @is_author = false
+    @most_viewed = Article.order(:views).last(3)
+
+    @most_viewed.each do |article|
+      p article
+    end
 
     if user_signed_in?
       @authors.each do |author|
@@ -12,6 +17,7 @@ class HomeController < ApplicationController
       end
     end
 
+    
   end
 
   def about
